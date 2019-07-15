28e9c51c5c25dab35dbac44c940ddbd03628773c
mcartagena <marcelocartagenaorellana@gmail.com>
2019-07-14 22:11:18 -0400

    strings and variables

---
+++ basics/strings.js
@@ -0,0 +1,14 @@
+let firstName = 'Marcelo';
+let lastName = 'Cartagena';
+let fullName = firstName + ' ' + lastName;
+
+console.log('name',fullName);
+
+// first challenge, we have three variables city, country and location, 
+// make location as string with a combination of the 2 initial variables
+
+let city = 'Quito';
+let country = 'Ecuador';
+let location = city +', '+country;
+
+console.log('location',location);
